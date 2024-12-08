896f1901020384b7290a0e3bc2bc2ce32d3822b1
chichi-a <kchic22@freeuni.edu.ge>
2024-12-08 14:46:30 +0400

    load commands

--- src/memory.py
+++ src/memory.py
@@ -42,37 +42,35 @@ def store_word(val_register,src_register,add_mem,curr_cpu):
     address = curr_cpu.registers[src_register]
     address += add_mem
 
-    if address + 3 > curr_cpu.total_size:
+    if address + 3 >= curr_cpu.total_size | address < 0 :
         raise ValueError("not enough space")
 
     value = curr_cpu.registers[val_register]
 
     #if i have value : 12345678
     #ill have to store : 78 56 34 12 
+    for i in range (0,4):
+        curr_cpu.memory_blocks[address+i] = nth_byte(value,i,4)
 
-    curr_cpu.memory_blocks[address] = nth_byte(value,0,4)
-    curr_cpu.memory_blocks[address+1] = nth_byte(value,1,4)
-    curr_cpu.memory_blocks[address+2] = nth_byte(value,2,4)
-    curr_cpu.memory_blocks[address+3]  = nth_byte(value,3,4)
     
 def store_half(val_register,src_register,add_mem,curr_cpu):
     address = curr_cpu.registers[src_register]
-    address += add_mem
+    address -= add_mem
 
-    if address + 1 > curr_cpu.total_size:
+    if address + 1 >= curr_cpu.total_size | address < 0 :
         raise ValueError("not enough space")
 
     value = curr_cpu.registers[val_register]
 
-    curr_cpu.memory_blocks[address] = nth_byte(value,0,2)
-    curr_cpu.memory_blocks[address+1] = nth_byte(value,1,2)
+    for i in range (0,2):
+        curr_cpu.memory_blocks[address+i] = nth_byte(value,i,4)
 
 
 def store_byte(val_register,src_register,add_mem,curr_cpu):
     address = curr_cpu.registers[src_register]
-    address += add_mem
+    address -= add_mem
 
-    if address  > curr_cpu.total_size:
+    if address  >= curr_cpu.total_size | address < 0:
         raise ValueError("not enough space")
 
     value = curr_cpu.registers[val_register]
@@ -80,8 +78,43 @@ def store_byte(val_register,src_register,add_mem,curr_cpu):
     curr_cpu.memory_blocks[address]  = nth_byte(value,0,1)
 
 
+def load_word(val_register,dest_register,add_mem,curr_cpu):
+    address = curr_cpu.registers[val_register]
+    address -= add_mem
 
+    if address + 3 >= curr_cpu.total_size | address < 0:
+        raise ValueError("not enough space")
 
+    values = []
+    for i in range(0,4):
+        values.append(curr_cpu.memory_blocks[address+i])
 
+    ans = conc_two_values(values[3],values[2])
+    ans = conc_two_values(ans,values[1])
+    ans = conc_two_values(ans,values[0])
 
+    curr_cpu.registers[dest_register] = ans
 
+def load_half(val_register,dest_register,add_mem,curr_cpu):
+    address = curr_cpu.registers[val_register]
+    address -= add_mem
+
+    if address + 1 >= curr_cpu.total_size | address < 0:
+        raise ValueError("not enough space")
+    
+    values = []
+    for i in range(0,2):
+        values.append(curr_cpu.memory_blocks[address+i])
+
+    ans = conc_two_values(values[1],values[0])
+    curr_cpu.registers[dest_register] = ans
+
+def load_byte(val_register,dest_register,add_mem,curr_cpu):
+    address = curr_cpu.registers[val_register]
+    address -= add_mem
+
+    if address  >= curr_cpu.total_size | address < 0:
+        raise ValueError("not enough space")
+    
+    value = curr_cpu.memory_blocks[address]
+    curr_cpu.registers[dest_register] = value
